40c349470155e51308a4d1b2ff8610da95ac3768
Zach Lennane <zach.lennane@pendo.io>
2024-10-10 15:29:26 -0400

    Update vite.config.ts to remove vueDevTools from production build

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,12 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-// Remove vueDevTools if not needed in production
 
-export default defineConfig(({ command, mode }) => ({
+export default defineConfig(() => ({
   base: '/',
   plugins: [
     vue(),
     vueJsx(),
-    // vueDevTools(), // Typically used only in development
   ],
   resolve: {
     alias: {
