accf87381abcb7e6f1cf35568aeb30e80687fa30
Zach Lennane <zach.lennane@pendo.io>
2024-11-04 14:39:32 -0500

    Add DOMContentLoaded to delay pendo snippet injection

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,19 @@ import './assets/styles.css'
 const app = createApp(App)
 app.use(router)
 
-// Safely check if Pendo exists and initialize it
-if (typeof window.pendo !== 'undefined') {
-  window.pendo.initialize({
-    visitor: {
-      id: 'zach.lennane@pendo.io' // Required if user is logged in
-      // email:        // Recommended if using Pendo Feedback, or NPS Email
-      // full_name:    // Recommended if using Pendo Feedback
-      // role:         // Optional
-    }
-  });
-} else {
-  console.warn(
-    'Pendo is not defined. Make sure the Pendo script is correctly included in index.html.'
-  );
-}
+document.addEventListener('DOMContentLoaded', () => {
+  // Check if Pendo is loaded and initialize
+  if (typeof window.pendo !== 'undefined') {
+    window.pendo.initialize({
+      visitor: {
+        id: 'zach.lennane@pendo.io', // Required if user is logged in
+      }
+    });
+  } else {
+    console.warn(
+      'Pendo is not defined. Make sure the Pendo script is correctly included in index.html.'
+    );
+  }
+});
 
 app.mount('#app')
